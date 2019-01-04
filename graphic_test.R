3a050c489cc936c9989d4e576b5c2ad5762fb0c1
Unknown <kevin97@orange.fr>
2019-01-04 23:54:26 +0100

    ajout des fichiers permettant de calculer les données demandé (moyenne, variance etc)

--- graphic_test.R
+++ graphic_test.R
@@ -1,4 +1,6 @@
+source('./sort.R')
 library(forcats)
+
 
 bitcoin_price_sorted_2016 <- bitcoin_price_sorted[bitcoin_price_sorted$Year == 2016,]
 bitcoin_price_sorted_2016 <- bitcoin_price_sorted_2016[order(bitcoin_price_sorted_2016$Day),]
@@ -6,7 +8,12 @@ bitcoin_price_sorted_2016 <- bitcoin_price_sorted_2016[order(bitcoin_price_sorte
 
 ##courbe de l'évolution du cours du bitcoin en 2016
 graph <- ggplot(bitcoin_price_sorted_2016, aes(x=fct_inorder(Date), y=Low, colour="blue", shape="square", group=1))
-graph <- graph + scale_x_continuous("Months", limits=c(0,30))
 graph <- graph + geom_line(size=1)
 print(graph)
-View(bitcoin_price_sorted_2016)
+##View(bitcoin_price_sorted_2016)
+
+catin<-summary(bitcoin_price_sorted$Year)
+compte <- table(bitcoin_price_sorted$Year)
+print(catin)
+barplot(compte)
+
